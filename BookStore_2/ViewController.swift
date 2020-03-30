a96d451075e10d6d29c5ef9bfa92d08b1e688112
Alejandro Hahn <30887815+alexhg128@users.noreply.github.com>
2020-03-30 14:52:34 -0700

    ACT4: Fixed delete function

--- BookStore_2/ViewController.swift
+++ BookStore_2/ViewController.swift
@@ -26,21 +26,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            do {
+                let book : Book = loadBooks()[indexPath.row]
+                managedObjectContext.delete(book)
+                try managedObjectContext.save()
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch let error as NSError {
+                NSLog("My Error: %@", error)
+            }
+            
+            
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
     
     @IBOutlet weak var myTableView : UITableView!
+    @IBOutlet weak var editButton : UIBarButtonItem!
+    @IBOutlet weak var toolbar : UINavigationItem!
     var managedObjectContext : NSManagedObjectContext!
     var ascending : Bool = true
+    var tvediting : Bool = false
+    var prefs: UserDefaults = UserDefaults.standard
+    var counter : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let appDelegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
         managedObjectContext = appDelegate.persistentContainer.viewContext as NSManagedObjectContext
+        myTableView.allowsMultipleSelectionDuringEditing = true
+        counter = prefs.integer(forKey: "counter")
+        rightButtons = toolbar.rightBarButtonItems!
     }
     
     @IBAction func addNew(_ sender: UIBarButtonItem) {
         let book: Book = NSEntityDescription.insertNewObject(forEntityName: "Book", into: managedObjectContext) as! Book
-        book.title = "My Book " + String(loadBooks().count)
+        counter += 1
+        book.title = "My Book " + String(counter)
+        prefs.synchronize()
         do {
             try managedObjectContext.save()
         } catch let error as NSError {
@@ -50,21 +77,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         myTableView.reloadData()
     }
     
-    @IBAction func deleteOne(_ sender: UIBarButtonItem) {
-        
-        let request: NSFetchRequest<Book> = Book.fetchRequest()
-        let pred = NSPredicate(format: "(title = %@)", "My Book " + String(loadBooks().count))
-        request.predicate = pred
-        
-        do {
-            let books : [Book] = try managedObjectContext.fetch(request as! NSFetchRequest<NSFetchRequestResult>) as! [Book]
-            managedObjectContext.delete(books[0])
-            try managedObjectContext.save()
-        } catch let error as NSError {
-            NSLog("My Error: %@", error)
+    var rightButtons : [UIBarButtonItem] = []
+    
+    
+    @IBAction func toogleEditing(_ sender: UIBarButtonItem?) {
+        tvediting = !tvediting;
+        myTableView.setEditing(tvediting, animated: true)
+        let button = UIBarButtonItem(barButtonSystemItem: tvediting ? UIBarButtonItem.SystemItem.done : UIBarButtonItem.SystemItem.edit, target: self, action: #selector(toogleEditing(_:)))
+        if tvediting {
+            let trash : [UIBarButtonItem] = [UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.trash, target: self, action: #selector(deleteSelected(_:)))]
+            toolbar.setRightBarButtonItems(trash, animated: true)
+            toolbar.setLeftBarButton(button, animated: true)
+        } else {
+            toolbar.setRightBarButtonItems(rightButtons, animated: true)
+            toolbar.setLeftBarButton(nil, animated: true)
         }
+    }
+    
+    @IBAction func deleteSelected(_ sender: UIBarButtonItem) {
         
-        myTableView.reloadData()
+        if let selectedRows = myTableView.indexPathsForSelectedRows {
+            let books : [Book] = loadBooks()
+            for indexPath in selectedRows  {
+                managedObjectContext.delete(books[indexPath.row])
+            }
+            do {
+                try managedObjectContext.save()
+                myTableView.beginUpdates()
+                myTableView.deleteRows(at: selectedRows, with: .automatic)
+                myTableView.endUpdates()
+                toogleEditing(nil)
+            } catch let error as NSError {
+                NSLog("My Error: %@", error)
+            }
+        }
     }
     
     @IBAction func changeOrder(_ sender: UIBarButtonItem) {
